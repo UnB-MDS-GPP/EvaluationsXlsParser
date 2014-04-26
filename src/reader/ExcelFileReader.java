040f953d80a02bebd7956b7343db39d70988e8be
Fabio Teixeira <fabio1079@gmail.com>
2014-04-26 15:01:33 -0300

    Class ExcelFileReader to read xls files using Apache POI

---
+++ src/reader/ExcelFileReader.java
@@ -0,0 +1,55 @@
+package reader;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Vector;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+
+public class ExcelFileReader {
+	private String fileName;
+
+	public ExcelFileReader(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public Vector readExcelFile() throws IOException {
+		// A vector os cells
+		Vector cellVectorHolder = new Vector();
+
+		// Creating Input Stream
+		FileInputStream myInput = new FileInputStream(this.fileName);
+		
+		// Create a POIFSFileSystem object
+		POIFSFileSystem myFileSystem = new POIFSFileSystem(myInput);
+		
+		// Create a workbook using the File System
+		HSSFWorkbook myWorkBook = new HSSFWorkbook(myFileSystem);
+		
+		// Get the first sheet from workbook
+		HSSFSheet mySheet = myWorkBook.getSheetAt(0);
+		
+		// Iterator to iterate through the cells
+		Iterator rowIter = mySheet.rowIterator();
+		
+		while(rowIter.hasNext()){
+            HSSFRow myRow = (HSSFRow) rowIter.next();
+            Iterator cellIter = myRow.cellIterator();
+            Vector cellStoreVector=new Vector();
+
+            while(cellIter.hasNext()){
+                HSSFCell myCell = (HSSFCell) cellIter.next();
+                cellStoreVector.addElement(myCell);
+            }
+
+            cellVectorHolder.addElement(cellStoreVector);
+        }
+
+		return cellVectorHolder;	
+	}
+}
