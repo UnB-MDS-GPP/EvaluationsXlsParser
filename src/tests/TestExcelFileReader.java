3b024d49085f9652537091e778c4e800add20938
Brenddon Gontijo <brenddongontijo@msn.com>
2014-05-04 11:09:36 -0300

    Created method shouldGetEvaluationFromXlsFile on TestExcelFileReader.java

--- src/tests/TestExcelFileReader.java
+++ src/tests/TestExcelFileReader.java
@@ -25,24 +25,41 @@ public class TestExcelFileReader {
 
 	@Test
 	public void shouldGetAreaReview() throws IOException{
-		ExcelFileReader excelFR = new ExcelFileReader();
+		ExcelFileReader excelFR_1 = new ExcelFileReader();
 		ExcelFileReader excelFR_2 = new ExcelFileReader();
 		
-		excelFR.setFileName("./src/xls/2007/Antropologia_Arqueologia.xls");
-		excelFR.setYear(2007);
+		excelFR_1.setFileName("./src/xls/2007/Antropologia_Arqueologia.xls");
+		excelFR_1.setYear(2007);
 		
 		excelFR_2.setFileName("./src/xls/2010/Artes_Musica.xls");
 		excelFR_2.setYear(2010);
 		
-		assertEquals(excelFR.getAreaReview(),"ANTROPOLOGIA / ARQUEOLOGIA");
+		assertEquals(excelFR_1.getAreaReview(),"ANTROPOLOGIA / ARQUEOLOGIA");
 		assertEquals(excelFR_2.getAreaReview(),"ARTES / MÚSICA");	
 	}
-	/*
+	
 	@Test
-	public void shouldGetEvaluationFromXlsFile(){
-		ArrayList<String> files = ListFiles.getAllXlsFilesFromFolder("../xls/2007/");
-		File folder = new File(path);
-		assertEquals(0, files.size());
-	}*/
+	public void shouldGetEvaluationFromXlsFile() throws IOException{
+		ExcelFileReader excelFR_1 = new ExcelFileReader();
+		ExcelFileReader excelFR_2 = new ExcelFileReader();
+		
+		excelFR_1.setFileName("./src/xls/2007/Antropologia_Arqueologia.xls");
+		excelFR_1.setYear(2007);
+		
+		excelFR_2.setFileName("./src/xls/2010/Direito.xls");
+		excelFR_2.setYear(2010);
+		
+		assertEquals(excelFR_1.getEvaluationFromXslFile().get(0).getAcronym(), "UFRJ");
+		assertEquals(excelFR_1.getEvaluationFromXslFile().get(0).getName(), "ANTROPOLOGIA SOCIAL");
+		
+		assertEquals(excelFR_1.getEvaluationFromXslFile().get(14).getAcronym(), "UFRJ");
+		assertEquals(excelFR_1.getEvaluationFromXslFile().get(14).getName(), "ARQUEOLOGIA");
+		
+		assertEquals(excelFR_2.getEvaluationFromXslFile().get(0).getAcronym(), "PUC/SP");
+		assertEquals(excelFR_2.getEvaluationFromXslFile().get(0).getName(), "DIREITO");
+		
+		assertEquals(excelFR_2.getEvaluationFromXslFile().get(58).getAcronym(), "URI");
+		assertEquals(excelFR_2.getEvaluationFromXslFile().get(58).getName(), "DIREITO");
+	}
 
 }
