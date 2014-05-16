ef7013cb422c9272f098f5c2481a63f8fcbb744e
jonnatas <jonatas_lenon@hotmail.com>
2014-05-16 15:20:21 -0300

    Refactoring the class DatabaseEvaluationBuilder
    Signed-off-by: Fabio Teixeira <fabio1079@gmail.com>

--- src/Database/DatabaseEvaluationBuilder.java
+++ src/Database/DatabaseEvaluationBuilder.java
@@ -17,73 +17,87 @@ public class DatabaseEvaluationBuilder {
 
 	int i, j;
 	DataBaseStructures dsStructures;
+	ArrayList<String> files;
+	
+	public boolean createListXlsRowFieldsStorage(int year) {
+		this.setXlsFilesFromFolder(year);
 
-	ArrayList<XlsRowFieldsStorage> xlsRowData;
-
-	public boolean createListXlsRowFieldsStorage(int ano) throws IOException,
-			ClassNotFoundException, SQLException {
-		ArrayList<String> files;
-		if (ListFiles.getAllXlsFilesFromFolder("./src/xls/" + ano + "/") != null) {
-
-			files = ListFiles
-					.getAllXlsFilesFromFolder("./src/xls/" + ano + "/");
-		} else {
-			files = new ArrayList<String>();
-		}
 		if (files.size() != 0) {
-
-			try {
-				dsStructures = new DataBaseStructures();
-				dsStructures.initDB();
-			} catch (Exception e) {
-				System.out.println("Nao pode criar o banco");
-				e.printStackTrace();
+			if (this.initializeDatabase()) {
+				this.passToDatabaseEachFile(year);
 			}
 
-			System.out.println("\t---=== " + ano + " ===---");
-			System.out.println("Total de arquivos de " + ano + ": "
-					+ files.size() + "\n");
-			for (i = 0; i < files.size(); i++) {
-				System.out.println("Salvando os dados do arquivo '"
-						+ files.get(i) + "' no banco.");
-				ExcelFileReader xlsReader = new ExcelFileReader(files.get(i),
-						ano);
-				try {
-					xlsRowData = xlsReader.getEvaluationFromXslFile();
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				System.out.println("Total de linhas do arquivo: "
-						+ xlsRowData.size());
-
-				for (j = 0; j < xlsRowData.size(); j++) {
-					BuildEvaluation buildE = new BuildEvaluation(
-							xlsRowData.get(j));
-
-					try {
-						buildE.saveToDatabase();
-						System.out.println("Foi salvo no banco a linha "
-								+ (j + 1) + ".");
-					} catch (Exception e) {
-
-						System.out
-								.println("Nao pode salvar no banco os dados da linha "
-										+ (j + 1)
-										+ " do arquivo '"
-										+ files.get(i) + "'");
-						e.printStackTrace();
-						break;
-					}
-
-				}
-			}
 			return true;
 		} else {
-			System.out.println("Ano não armazenado");
+			System.out.println("The year "+year+" could not be saved");
 			return false;
+		}
+	}
+	
+	private void setXlsFilesFromFolder(int year) {
+		if (ListFiles.getAllXlsFilesFromFolder("./src/xls/" + year + "/") != null) {
+			this.files = ListFiles.getAllXlsFilesFromFolder("./src/xls/" + year + "/");
+		} else {
+			this.files = new ArrayList<String>();
+		}
+	}
+
+	private boolean initializeDatabase() {
+		boolean initialized;
+
+		try {
+			dsStructures = new DataBaseStructures();
+			dsStructures.initDB();
+			initialized = true;
+		} catch (Exception e) {
+			System.out.println("Could not create the database");
+			e.printStackTrace();
+			initialized = false;
+		}
+
+		return initialized;
+	}
+
+	private void passToDatabaseEachFile(int year) {
+		ArrayList<XlsRowFieldsStorage> xlsRowData = new ArrayList<XlsRowFieldsStorage>();
+		System.out.println("\t---=== " + year + " ===---");
+		System.out.println("Total de arquivos de " + year + ": "+ files.size() + "\n");
+
+		for (i = 0; i < files.size(); i++) {
+			System.out.println("Saving the data from file '"+ files.get(i) +"' into database.");
+
+			ExcelFileReader xlsReader = new ExcelFileReader(files.get(i), year);
+
+			try {
+				xlsRowData = xlsReader.getEvaluationFromXslFile();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+
+			System.out.println("Total lines from archive: "+ xlsRowData.size());
+
+			this.passToDataBaseAllRowsFromAFile(xlsRowData);
 		}
 
 	}
 
+	private void passToDataBaseAllRowsFromAFile(ArrayList<XlsRowFieldsStorage> xlsRowData) {
+		for (j = 0; j < xlsRowData.size(); j++) {
+			BuildEvaluation buildE = new BuildEvaluation(xlsRowData.get(j));
+
+			try {
+				buildE.saveToDatabase();
+				System.out.println("Saved into the database the row "+ (j + 1) + ".");
+			} catch (Exception e) {
+				System.out.println(
+						"Could not save into the database the row"
+						+ (j + 1)
+						+ " of the file '"
+						+ files.get(i) + "'");
+
+				e.printStackTrace();
+				break;
+			}
+		}
+	}
 }
